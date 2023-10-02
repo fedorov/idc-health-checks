8ddb2762fab9fdf3402cbb49a104d0817dd6b895
Andrey Fedorov <andrey.fedorov@gmail.com>
2023-10-02 15:55:10 -0400

    add first query check

--- idc_tests.py
+++ idc_tests.py
@@ -1,8 +1,19 @@
 import unittest
+from google.cloud import bigquery
+
+bq_queries = {
+"check_if_table_exists": "SELECT 1 FROM `biquery-public-data.idc_current.dicom_all` LIMIT 1",
+
+}
+
 
 class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(fun(3), 4)
+    def test_bq_queries(self):
+        # iterate over all queries in bq_queries dictionary and execute each query
+        for query_name,query in bq_queries.items():
+            print("Executing query: " + query_name + " with query: " + query + " ...")
+            client = bigquery.Client()
+            client.query(bq_queries[query]).result()
 
 if __name__ == '__main__':
     unittest.main()
