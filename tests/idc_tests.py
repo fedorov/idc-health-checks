143c8d2181289fedcc5a2ab289c8bdfcf4007015
Andrey Fedorov <andrey.fedorov@gmail.com>
2023-10-02 16:36:21 -0400

    add API tests

--- tests/idc_tests.py
+++ tests/idc_tests.py
@@ -1,6 +1,8 @@
 import unittest
 from google.cloud import bigquery
 import os
+import requests
+import json
 
 bq_queries = {
 "check_if_table_exists": """
@@ -11,7 +13,11 @@ bq_queries = {
 
 }
 
+idc_api_preamble = "https://api.imaging.datacommons.cancer.gov/v1"
+idc_dev_api_preamble = "https://dev-api.canceridc.dev/v2"
 
+def pretty(response):
+    print(json.dumps(response.json(), sort_keys=True, indent=4))
 
 class MyTest(unittest.TestCase):
     def test_bq_queries(self):
@@ -21,5 +27,27 @@ class MyTest(unittest.TestCase):
             client = bigquery.Client(os.environ["PROJECT_ID"])
             client.query(bq_queries[query_name]).result()
 
+    def test_prod_api(self):
+        response = requests.get('{}/collections'.format(idc_api_preamble))
+        # Check that there wasn't an error with the request
+        if response.status_code != 200:
+            # Print the error code and message if something went wrong
+            print('Request failed: {}'.format(response.reason))
+
+        # Print the collections JSON text
+        pretty(response)
+
+    def test_dev_api(self):
+        response = requests.get('{}/collections'.format(idc_dev_api_preamble))
+        # Check that there wasn't an error with the request
+        if response.status_code != 200:
+            # Print the error code and message if something went wrong
+            print('Request failed: {}'.format(response.reason))
+        
+        # Print the collections JSON text
+        pretty(response)
+
+# Print the collections JSON text
+pretty(response)
 if __name__ == '__main__':
     unittest.main()
