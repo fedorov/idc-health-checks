89250fc4cc5032e4ed1b2bab45882d8c12cbd97f
Andrey Fedorov <andrey.fedorov@gmail.com>
2023-10-02 16:05:19 -0400

    add project ID to the env

--- tests/idc_tests.py
+++ tests/idc_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from google.cloud import bigquery
+import os
 
 bq_queries = {
 "check_if_table_exists": "SELECT 1 FROM `biquery-public-data.idc_current.dicom_all` LIMIT 1",
@@ -12,7 +13,7 @@ class MyTest(unittest.TestCase):
         # iterate over all queries in bq_queries dictionary and execute each query
         for query_name,query in bq_queries.items():
             print("Executing query: " + query_name + " with query: " + query + " ...")
-            client = bigquery.Client()
+            client = bigquery.Client(os.environ["PROJECT_ID"])
             client.query(bq_queries[query_name]).result()
 
 if __name__ == '__main__':
